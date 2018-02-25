ff07724f5a848155cc9581a956dda26c076f51c1
Jon Wood <jon@blankpad.net>
2018-02-25 21:50:21 +0000

    Refactor to tidy things up

--- chapter_3/temperature_converter/src/main.rs
+++ chapter_3/temperature_converter/src/main.rs
@@ -1,18 +1,21 @@
 use std::io::{self, Write};
 
 fn main() {
-    let units = get_input("Units (c/f): ");
-    let units = units.trim();
+    let unit = get_input("Units (c/f): ");
+    let unit = unit.trim();
+
     let value: f32 = get_input("Value: ").trim().parse().unwrap();
 
-    if units == "c" {
-        let farenheit = value * 9.0 / 5.0 + 32.0;
-        println!("{}c is {}f", value, farenheit);
-    } else if units == "f" {
-        let celcius = (value - 32.0) / (9.0 / 5.0);
-        println!("{}f is {}c", value, celcius);
-    } else {
-        println!("Unknown units: {}", units);
+    match unit {
+        "c" => {
+            let farenheit = value * 9.0 / 5.0 + 32.0;
+            println!("{}c is {}f", value, farenheit);
+        },
+        "f" => {
+            let celcius = (value - 32.0) / (9.0 / 5.0);
+            println!("{}f is {}c", value, celcius);
+        },
+        _ => println!("Invalid unit {}", unit),
     }
 }
 

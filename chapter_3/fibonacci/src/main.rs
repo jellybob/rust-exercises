1c97bff81895c830d1acab4ff70c37bf20433c41
Jon Wood <jon@blankpad.net>
2018-02-26 09:24:56 +0000

    Fibonacci solver

--- chapter_3/fibonacci/src/main.rs
+++ chapter_3/fibonacci/src/main.rs
@@ -1,3 +1,31 @@
+use std::env;
+
 fn main() {
-    println!("Hello, world!");
+    if env::args().count() != 2 {
+        println!("Usage: fibonacci number");
+        std::process::exit(1);
+    }
+
+    let number = match env::args().last() {
+        Some(x) => x,
+        None => String::new(),
+    };
+
+    let number: u32 = match number.parse() {
+        Ok(x) => x,
+        Err(_) => {
+            println!("That should be a number");
+            0
+        },
+    };
+
+    println!("fib({}) == {}", number, fib(number));
+}
+
+fn fib(n: u32) -> u32 {
+    if n < 2 {
+        return n
+    } else {
+        return fib(n-1) + fib(n-2);
+    }
 }
